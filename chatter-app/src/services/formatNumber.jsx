bd9f6c67c6b72669b3f252b7ded8063ed2fc7aa9
Arth <arthsalgia@gmail.com>
2026-07-31 17:23:18 +0530

    fix format number

--- chatter-app/src/services/formatNumber.jsx
+++ chatter-app/src/services/formatNumber.jsx
@@ -1,5 +1,5 @@
 export default function formatNumber(num) {
-    const lastPart = num.slice(-10, -1);
+    const lastPart = num.slice(-10);
     const firstPart = num.slice(0, num.length -10)
     return firstPart + " " + lastPart
 }
